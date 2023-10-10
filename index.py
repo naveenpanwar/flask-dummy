ea744fe18cf3e180e23c7fc6793d8aa43065923f
Naveen Panwar <naveen.panwar27@gmail.com>
2023-10-10 15:43:32 +0530

    Initial commit

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.post("/get-session-token")
+def get_session_token():
+    return "Some answer."
+
+
+if __name__ == "__main__":
+    app.run()
